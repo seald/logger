837bb35fb6841c19ed6403e5d3bc0b95c86013d5
Timothée Rebours <t.rebours@gmail.com>
2017-02-22 18:44:45 +0100

    stringify message parts with JSON.stringify if not strings

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const cache = {}
 const logger = (level, namespace) => (...messages) => {
   let message
   try {
-    message = messages.map(_message => JSON.stringify(_message)).join(' ')
+    message = messages.map(_message => typeof _message === 'string' ? _message : JSON.stringify(_message)).join(' ')
   } catch (error) {
     level = 2
     message = `Couldn't output log because ${error.message} at ${error.stack}`
