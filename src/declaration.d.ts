4ebd1d729fff3294243ea366a2b1d982af79c125
clement <clement@seald.io>
2021-05-19 15:35:31 +0200

    add missing types

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,6 +1,10 @@
 export type makeLogger = (namespace: string) => ({
-  debug: (log: string) => void,
-  info: (log: string) => void,
-  warn: (log: string) => void,
-  error: (log: string) => void,
+  debug: (message?: any, ...optionalParams: any[]) => void
+  info: (message?: any, ...optionalParams: any[]) => void
+  warn: (message?: any, ...optionalParams: any[]) => void
+  error: (message?: any, ...optionalParams: any[]) => void
 })
+
+export type setLogLevel = (logLevel: string) => void
+export type setHistorySize = (size: number) => void
+export type flushToString = () => string
